97c31dc8e8bb5a01338d3d13b7f1ca65a69e7373
Taiga Sasaki <toratiger5286@gmail.com>
2026-09-26 16:54:43 +0900

    Physicsに最高速度を設定する機能を追加

--- Project/Source/Components/Physics.cpp
+++ Project/Source/Components/Physics.cpp
@@ -2,6 +2,7 @@
 #include <algorithm>
 #include <cassert>
 #include "Transform.h"
+#include "Utility/Vector2.h"
 
 void Physics::Init(Transform* pTransform, float drag, float gravity)
 {
@@ -18,12 +19,23 @@ void Physics::Init(Transform* pTransform, float drag, float gravity)
 
 void Physics::Update()
 {
+	// 速度に加速度を足す
 	m_vel += m_accel;
 	m_vel.y += m_gravity;
 
+	// 水平移動は減衰する
 	m_vel.x *= m_drag;
 	m_vel.z *= m_drag;
 
+	// 水平移動の最高速度を設定
+	Vector2 velXZ = Vector2(m_vel.x, m_vel.z);
+	if (velXZ.SquaredLength() > m_maxSpeed * m_maxSpeed)
+	{
+		m_vel.x = velXZ.x;
+		m_vel.z = velXZ.y;
+	}
+
+	// 位置に速度を足す
 	m_pTransform->pos += m_vel;
 }
 
--- Project/Source/Components/Physics.h
+++ Project/Source/Components/Physics.h
@@ -10,6 +10,7 @@ class Physics :
 public:
     static constexpr float kDefaultDrag = 0.9f;
     static constexpr float kDefaultGravity = -1.0f;
+    static constexpr float kDefaultMaxSpeed = 30.0f;
 
     void Init(Transform* pTransform,float drag = kDefaultDrag,float gravity = kDefaultGravity);
     void Update();
@@ -21,12 +22,17 @@ public:
     void SetGravity(float gravity) { m_gravity = gravity; }
     float GetGravity() const { return m_gravity; }
 
+    void SetMaxSpeed(float maxSpeed) { m_maxSpeed = maxSpeed; }
+    float GetMaxSpeed() const { return m_maxSpeed; }
+
     Vector3 m_vel;  // posに毎フレーム足す速度
     Vector3 m_accel;    // velに毎フレーム足す加速度
 
 private:
     Transform* m_pTransform = nullptr;  // Transformの参照
 
-    float m_drag = kDefaultDrag;    // velに毎フレーム掛ける速度の減衰率(0.0~1.0)(1.0にすると減衰しない)
+    float m_drag = kDefaultDrag;    // velに毎フレーム掛ける速度の減衰率(0.0~1.0)(1.0にすると減衰しない)(XZ成分にのみ有効)
     float m_gravity = -kDefaultGravity;    // vel.yに毎フレーム足す重力加速度(0にすると重力なし)
+    // 最高速度(XZ成分にのみ有効)
+    float m_maxSpeed = kDefaultMaxSpeed;
 };
